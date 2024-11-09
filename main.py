4f0e6c1c2f422d8b03d64e274d3f002e1aee8c74
ВИТАЛИЙ КАРЯКИН <karyakin01@gmail.com>
2024-11-09 17:27:05 +0300

    Практическая работа по уроку "Динамическая типизация"

---
+++ main.py
@@ -0,0 +1,12 @@
+# Практическая работа по уроку "Динамическая типизация"
+name = 'Vitaly'
+print ('Name:' , name)
+
+age = 53
+print ('Ago:' , age)
+
+age = 53+5
+print('New Age:' , age)
+
+is_student = True
+print('is_student:' , is_student )
